9599cb714e2002fbd3204ed4505a673fa4d44a47
Ankur Shahi <ankurshahi@MacBook-Air.local>
2021-09-06 01:21:47 -0400

    initial commit and the About section

---
+++ src/components/About/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import displayImage from "../../assets/displayImage.jpeg"
+const About=()=>{
+    return(
+        <section className="my-5">
+            <h1 id="about">About Me</h1>
+            <img 
+                src={displayImage} 
+                className="my-2" 
+                style={{borderRadius:"50%", width:"15%"}}
+                alt="Ankur Shahi"
+            />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consectetur voluptas adipisci eveniet expedita dolor, quaerat commodi quidem ipsa illum inventore harum officia. Animi omnis veniam ad voluptas dolores officia!
+                <br/>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consectetur voluptas adipisci eveniet expedita dolor, quaerat commodi quidem ipsa illum inventore harum officia. Animi omnis veniam ad voluptas dolores officia!
+            </p>
+        </section>
+    )
+}
+
+export default About;
