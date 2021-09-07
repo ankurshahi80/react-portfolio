6431f23882f5700c53c46d890647a2729b73f629
Ankur <ankurshahi@Ankurs-iMac.local>
2021-09-07 01:43:47 -0400

    Contact and Resume sections updated

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,42 +1,13 @@
 import React from 'react';
-import linkedIn from '../../assets/LI-In-Bug.png';
-import GitHub from '../../assets/GitHub-logo.png';
 
-const Footer =()=>{
-    const handleClick=()=>{
-    console.log("click handled");
-    };
-
-    return(
-        <footer >
-            <div className="footer-wrapper">
-                
-                    {/* <ul className="flex-row">
-                        <li>
-                            <span onClick={() => handleClick()}><img src={linkedIn} style={{width:"10%"}} alt="linkedIn"></img></span>
-                        </li>
-                        <li>
-                            <span onClick={() => handleClick()}><img src={GitHub} style={{width:"10%", backgroundColor:"white"}}></img></span>
-                        </li>
-                        <li>
-                            <span onClick={() => handleClick()}>Other</span>
-                        </li>
-                    </ul> */}
-                    <ul className="logo-container">
-                        <li>
-                            <span onClick={() => handleClick()}><img src={linkedIn} style={{maxwidth:"33%"}} alt="linkedIn"></img></span>
-                        </li>
-                        <li>
-                            <span onClick={() => handleClick()}><img src={GitHub} style={{width:"33%", backgroundColor:"white"}}></img></span>
-                        </li> 
-                        <li>
-                        <span onClick={() => handleClick()}><img src={linkedIn} style={{maxwidth:"33%"}} alt="linkedIn"></img></span>
-                        </li>
-                    </ul>
-                
-            </div>
-        </footer>
+function Footer() {
+    return (
+      <div className="contact-icons">
+          <a href="tel:647-388-2109"><span>Phone</span></a>
+          <a href="mailto:shahiankur80@gmail.com"><span>Email</span></a>
+          <a href="https://github.com/ankurshahi80" target="_blank" rel="noreferrer"><span>Github</span></a>
+          <a href="https://www.linkedin.com/in/ankur-shahi/" target="_blank" rel="noreferrer"><span>LinkedIn</span></a>
+        </div>
     )
-};
-
+  }
 export default Footer;
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,8 +1,59 @@
 import React from 'react';
 
+import resumePDF from '../../assets/AnkurShahi.pdf';
+
 const Resume = ()=>{
     return(
-        <div>resume</div>
+        <section className="resume-section">
+            <h1>Resume</h1>
+            <div>
+                <h2>Work History</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consectetur voluptas adipisci eveniet expedita dolor, quaerat commodi quidem ipsa illum inventore harum officia. Animi omnis veniam ad voluptas dolores officia!</p>
+            </div>
+            <div>
+                <h2>Technical Skills</h2>
+                <ul>
+                    <li>
+                        <p>HTML</p>
+                    </li>
+                    <li>
+                        <p>CSS</p>
+                    </li>
+                    <li>
+                        <p>JavaScript</p>
+                    </li>
+                    <li>
+                        <p>Node.js</p>
+                    </li>
+                    <li>
+                        <p>Express.js</p>
+                    </li>
+                    <li>
+                        <p>React</p>
+                    </li>
+                    <li>
+                        <p>SQL, mySQL2</p>
+                    </li>
+                    <li>
+                        <p>noSQL, MongoDB</p>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h2>Education History</h2>
+                <ul>
+                    <li>Program: Coding Boot Camp</li>
+                    <li>Year: 2021</li>
+                    <li>University: University of Toronto</li>
+                </ul>
+            </div>
+            
+        <div className="resume-download">
+            <h3 className="title-3">Download My Resume.</h3>
+            <p>If you would like to know more, please download my resume!</p>
+            <a className="downloadButton" href={resumePDF} download="resume_ankur.pdf">Download PDF</a>
+        </div>
+    </section>
     )
 };
 
