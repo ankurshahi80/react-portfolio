18d34be8bc8480c00ecc424bb51787484d68a2dc
Ankur <ankurshahi@Ankurs-iMac.local>
2021-09-26 19:05:43 -0400

    react-icons added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { AiFillGithub } from 'react-icons/ai';
 
 function Footer() {
     return (
       <div className="contact-icons">
           <a href="tel:647-388-2109"><span>Phone</span></a>
           <a href="mailto:shahiankur80@gmail.com"><span>Email</span></a>
-          <a href="https://github.com/ankurshahi80" target="_blank" rel="noreferrer"><span>Github</span></a>
+          <a href="https://github.com/ankurshahi80" target="_blank" rel="noreferrer"><span><AiFillGithub/>Github</span></a>
           <a href="https://www.linkedin.com/in/ankur-shahi/" target="_blank" rel="noreferrer"><span>LinkedIn</span></a>
         </div>
     )
